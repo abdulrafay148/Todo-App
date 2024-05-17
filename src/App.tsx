fdae1f5e7411e8f3cd91f22149cc10d64ede20d8
abdulrafay148 <167872766+abdulrafay148@users.noreply.github.com>
2024-05-17 03:31:54 -0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [txt, settxt] = useState('Abc');
+  const [txtlist, setxtlist] = useState<string[]>([]);
+
+  const del = (i: number) => {
+    const newList = [...txtlist];
+    newList.splice(i, 1);
+    setxtlist(newList);
+  };
+
+  const edi = (index: number) => { 
+    const newtxt = prompt("Enter your new text");
+    if (newtxt !== null) {
+      const newlist = [...txtlist];
+      newlist[index] = newtxt;
+      setxtlist(newlist);
+    }
+  };
+
+  const addText = () => {
+    setxtlist([...txtlist, txt]);
+    settxt('');
+  };
+
+  const deleteAll = () => {
+    setxtlist([]);
+  };
+
   return (
+    <center>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      {/* <h4 id='h'>{txt}</h4> */}
+      <input 
+      value={txt}
+        type="text" 
+        id='inp1' 
+        onChange={(e) => {
+          settxt(e.target.value);
+          console.log(e.target.value);
+        }}
+      />
+      <button 
+        id='but1' 
+        className='add' 
+        onClick={addText}
+      >
+        Add
+      </button>
+      <button 
+        id='but2' 
+        className='del' 
+        onClick={deleteAll}
+      >
+        Delete All
+      </button>
+      {txtlist.map((x, i) => (
+        <div key={i} className='item'>
+          <span className='sp'>{x}</span>
+          <button onClick={() => del(i)} className='del'>Delete</button>
+          <button onClick={() => edi(i)} className='edi'>Edit</button>
+        </div>
+      ))}
+
     </div>
+    </center>
   );
 }
 
